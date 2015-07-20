924cd875e9c43b3bdf11745ef657a91f51c76d08
HugoViala <hugo.viala@gmail.com>
2015-07-19 22:29:32 -0800

    Learning script written

--- src/learn.py
+++ src/learn.py
@@ -2,9 +2,10 @@
 
 import sys
 import json
+import os.path
 
-def DataLearnedFilePath(lang):
-    return "../data/" + lang + "data.json"
+def DataLearnFilePath(lang):
+    return "../data/" + lang + "/data.json"
 
 # NOTE(hugo): this script needs several arguments
 # 0 - the python script (here learn.py)
@@ -13,25 +14,36 @@ def DataLearnedFilePath(lang):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         DataLearnFileName = DataLearnFilePath(sys.argv[1])
+        print DataLearnFileName
         Data = {}
-        if os.path.isfile(DataLearnFileName):
-            Data = json.loads(open(DataLearnFileName, "r+").read())
+        DataFile = open(DataLearnFileName, "r+");
+        Data = json.loads(DataFile.read())
         Book = open(sys.argv[2], "r").read().split()
         LastWord = ""
         for Word in Book:
             if Word in Data:
                 Data[Word]["occ"] += 1
             else:
+                Data[Word] = {}
+                Data[Word]["suc"] = {}
                 Data[Word]["occ"] = 1
             if LastWord != "":
-                Data[LastWord]["suc"]
+                if Word in Data[LastWord]["suc"]:
+                    Data[LastWord]["suc"][Word] += 1
+                else:
+                    Data[LastWord]["suc"][Word] = 1
 
             LastWord = Word
+
+        # NOTE(hugo): All data gathered. Time to write in file
+        DataFile.seek(0)
+        DataFile.write(json.dumps(Data, indent=4, separators=(' , ', ' : ')))
+
             
         
-                
+        print "Sucessfully wrote data in file"
         
     else:
         print "Misuse of the script"
